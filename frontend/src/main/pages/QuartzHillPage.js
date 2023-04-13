6689ec5ba4fac03a0bcf30f0ea0de7b8a2d7d928
Christian Baker <christianjbaker@icloud.com>
2023-04-12 19:43:01 -0700

    cb - add page for Quartz Hill

---
+++ frontend/src/main/pages/QuartzHillPage.js
@@ -0,0 +1,18 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function QuartzHillPage() {
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Quartz Hill, CA</h1>
+        <p>
+         <b>Quartz Hill, California</b> is a small residential town
+         located in Los Angeles County, California. It is home to about
+         10,000 people and is known for its high desert climate and scenic
+         views of the mountains.
+        </p>
+        <p>Added by Christian B.</p>
+      </div>
+    </BasicLayout>
+  )
+}
