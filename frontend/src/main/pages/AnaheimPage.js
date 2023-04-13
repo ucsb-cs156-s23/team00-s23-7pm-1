4a602a655e5d97cb9d317b1d1f7b093591490bf3
Hinojosa, Ricardo <rhinojosa1@go.pasadenua.edu>
2023-04-12 19:50:29 -0700

    RH - add page for Anaheim

---
+++ frontend/src/main/pages/AnaheimPage.js
@@ -0,0 +1,19 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function AnaheimPage() {
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Anaheim, CA</h1>
+        <p>
+         <b>Anaheim, California</b> is a city loacated in Southen California.
+         The city is known for being home to Disneyland, one of the world's most popular theme parks.
+         The city offers other attractions such as Angel Stadium, the home field of the  Los Angeles Angels, 
+         and offers vast options of cullinary experinences.
+
+        </p>
+        <p>Added by Ricardo H.</p>
+      </div>
+    </BasicLayout>
+  )
+}
